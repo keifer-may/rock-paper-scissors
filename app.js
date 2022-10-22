c83c8ed6ea0e9072459f2efbe106eac0ae329922
Keifer May <dkmay3@gmail.com>
2022-10-22 11:48:59 -0400

    Add function to check if player wins or loses
    Logs the player choice and computer choice and the result

--- app.js
+++ app.js
@@ -4,13 +4,49 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-getComputerChoice();
-
 function getPlayerChoice() {
   let choice = prompt(`Please pick from rock, paper or scissors: `);
-  let titleCase = choice.slice(0, 1).toUpperCase + choice.slice(1).toLowerCase;
+  let titleCase = choice.slice(0, 1).toUpperCase() + choice.slice(1).toLowerCase();
   return titleCase;
 }
 
-getPlayerChoice();
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection){
+        let outcome = `You tied!`;
+        return outcome;
+    }
+    else if (playerSelection === choices[0] && computerSelection === choices[1]){
+        let outcome = `You lose!`;
+        return outcome;
+    }
+    else if (playerSelection === choices[0] && computerSelection === choices[2]){
+        let outcome = `You win!`;
+        return outcome;
+    }
+    else if (playerSelection === choices[1] && computerSelection === choices[0]){
+        let outcome = `You win!`;
+        return outcome;
+    }
+    else if (playerSelection === choices[1] && computerSelection === choices[2]){
+        let outcome = `You lose!`;
+        return outcome;
+    }
+    else if (playerSelection === choices[2] && computerSelection === choices[0]){
+        let outcome = `You lose!`;
+        return outcome;
+    }
+    else if (playerSelection === choices[2] && computerSelection === choices[1]){
+        let outcome = `You win!`;
+        return outcome;
+    }
+}
 
+
+
+const computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice();
+
+
+console.log(playerSelection)
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection));
