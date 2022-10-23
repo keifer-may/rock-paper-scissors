d4c33307411e8d9f7747f96778a1c83caa3c4885
Keifer May <dkmay3@gmail.com>
2022-10-23 19:10:46 -0400

    Reformat files

--- app.js
+++ app.js
@@ -6,46 +6,59 @@ function getComputerChoice() {
 
 function getPlayerChoice() {
   let choice = prompt(`Please pick from rock, paper or scissors: `);
-  let titleCase = choice.slice(0, 1).toUpperCase() + choice.slice(1).toLowerCase();
+  let titleCase =
+    choice.slice(0, 1).toUpperCase() + choice.slice(1).toLowerCase();
   return titleCase;
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection){
-        let outcome = `You tied!`;
-        return outcome;
-    }
-    else if (playerSelection === choices[0] && computerSelection === choices[1]){
-        let outcome = `You lose!`;
-        return outcome;
-    }
-    else if (playerSelection === choices[0] && computerSelection === choices[2]){
-        let outcome = `You win!`;
-        return outcome;
-    }
-    else if (playerSelection === choices[1] && computerSelection === choices[0]){
-        let outcome = `You win!`;
-        return outcome;
-    }
-    else if (playerSelection === choices[1] && computerSelection === choices[2]){
-        let outcome = `You lose!`;
-        return outcome;
-    }
-    else if (playerSelection === choices[2] && computerSelection === choices[0]){
-        let outcome = `You lose!`;
-        return outcome;
-    }
-    else if (playerSelection === choices[2] && computerSelection === choices[1]){
-        let outcome = `You win!`;
-        return outcome;
-    }
+  if (playerSelection === computerSelection) {
+    let outcome = `You tied!`;
+    return outcome;
+  } else if (
+    playerSelection === choices[0] &&
+    computerSelection === choices[1]
+  ) {
+    let outcome = `You lose!`;
+    return outcome;
+  } else if (
+    playerSelection === choices[0] &&
+    computerSelection === choices[2]
+  ) {
+    let outcome = `You win!`;
+    return outcome;
+  } else if (
+    playerSelection === choices[1] &&
+    computerSelection === choices[0]
+  ) {
+    let outcome = `You win!`;
+    return outcome;
+  } else if (
+    playerSelection === choices[1] &&
+    computerSelection === choices[2]
+  ) {
+    let outcome = `You lose!`;
+    return outcome;
+  } else if (
+    playerSelection === choices[2] &&
+    computerSelection === choices[0]
+  ) {
+    let outcome = `You lose!`;
+    return outcome;
+  } else if (
+    playerSelection === choices[2] &&
+    computerSelection === choices[1]
+  ) {
+    let outcome = `You win!`;
+    return outcome;
+  }
 }
 
 for (let i = 0; i < 5; i++) {
-    const computerSelection = getComputerChoice();
-    const playerSelection = getPlayerChoice();
-        
-    console.log(playerSelection)
-    console.log(computerSelection)
-    console.log(playRound(playerSelection, computerSelection));
+  const computerSelection = getComputerChoice();
+  const playerSelection = getPlayerChoice();
+
+  console.log(playerSelection);
+  console.log(computerSelection);
+  console.log(playRound(playerSelection, computerSelection));
 }
