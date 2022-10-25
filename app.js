fe87a98b49ff118791f0b094a196158d04fa2ded
Keifer May <dkmay3@gmail.com>
2022-10-25 18:23:06 -0400

    Create play button & begin style
    Created a button that allows you to begin playing
    once you have finished the 5 rounds. Also began the
    process of styling the page.

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const choices = [`Rock`, `Paper`, `Scissors`];
-const buttons = document.querySelectorAll('.game-buttons #button img');
-const result = document.querySelector('.results h2');
+const buttons = document.querySelectorAll(".game-buttons #button img");
+const result = document.querySelector(".results h2");
+const computerResults = document.querySelector(".computer-score");
+const playerResults = document.querySelector(".player-score");
+const gameover = 5;
+const finalScore = document.querySelector(".final-score");
+const startGameButton = document.getElementById("play-button");
+
 let computerScore = 0;
 let playerScore = 0;
-const computerResults = document.querySelector('.computer-score');
-const playerResults = document.querySelector('.player-score');
-computerResults.textContent = `Computer: ${computerScore}`;
-playerResults.textContent = `Player: ${playerScore}`;
-const gameover = 5;
-const finalScore = document.querySelector('.final-score');
-let playing = true;
+let playing = false;
+let gamesPlayed = 0;
+
+function startPlaying(numberOfGames) {
+  if (numberOfGames === 0) {
+    computerResults.textContent = `Computer: ${computerScore}`;
+    playerResults.textContent = `Player: ${playerScore}`;
+    playing = true;
+  } else {
+    computerScore = 0;
+    playerScore = 0;
+    computerResults.textContent = `Computer: ${computerScore}`;
+    playerResults.textContent = `Player: ${playerScore}`;
+    playing = true;
+    finalScore.textContent = "";
+    result.textContent = "";
+  }
+}
 
 function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
@@ -64,19 +81,35 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-buttons.forEach(button => {
-  button.addEventListener('click', e => {
-      if (playing === true){
+startGameButton.textContent = `Would you like to play to the best of ${gameover} games?`;
+
+startGameButton.addEventListener("click", (e) => {
+  if (playing === false) {
+    startPlaying(gamesPlayed);
+    startGameButton.style.display = 'none';
+  }
+});
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (playing === true) {
       let play = playRound(e.target.classList.value, getComputerChoice());
       result.textContent = play;
       computerResults.textContent = `Computer: ${computerScore}`;
       playerResults.textContent = `Player: ${playerScore}`;
-      if (computerScore === gameover){
-        finalScore.textContent = 'You lost. Where can we find a hero?';
+      if (computerScore === gameover) {
+        finalScore.textContent = "You lost. Where can we find a hero?";
+        gamesPlayed++;
         playing = false;
-      }
-      else if (playerScore === gameover) {
-        finalScore.textContent = 'You have triumphed!';
+        startGameButton.style.display = 'block';
+        startGameButton.textContent = `Would you like to try another ${gameover} games?`;
+      } else if (playerScore === gameover) {
+        finalScore.textContent = "You have triumphed!";
+        gamesPlayed++;
         playing = false;
+        startGameButton.style.display = 'block';
+        startGameButton.textContent = `Would you like to try another ${gameover} games?`;
       }
-    }})});
+    }
+  });
+});
